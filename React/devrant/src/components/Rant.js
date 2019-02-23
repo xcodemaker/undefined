a7214ec57b481dbc0514c0ffbd7d7f8f64aaad03
Nadun Chamikara <djnadunc@gmail.com>
2019-02-23 19:24:46 +0530

    react-36

--- React/devrant/src/components/Rant.js
+++ React/devrant/src/components/Rant.js
@@ -8,20 +8,22 @@ import React, {Component} from 'react';
 class Rant extends Component {
 
     render() {
+        let rant = this.props.rant;
+
         return (
             <article className="post">
                 <div className="post__inner">
                     <div className="score">
-                        <div className="score__up layout--center">++</div>
-                        <div className="score__board layout--center">100</div>
-                        <div className="score__down layout--center">--</div>
+                        <div className={`score__up layout--center ${rant.myVote==1?'checked':''}`}>++</div>
+                        <div className="score__board layout--center">{rant.votes}</div>
+                        <div className={`score__down layout--center ${rant.myVote==-1?'checked':''}`}>--</div>
                     </div>
                     <div className="post__body">
-                        Hello World...
+                        {rant.content}
                     </div>
                 </div>
                 <div className="post__footer">
-                    <div className="post__time">2m ago</div>
+                    <div className="post__time">{rant.displayTime}</div>
                     <div className="post__comments">
                         <svg className="icon" viewBox="0 0 31 32">
                             <path d="M24.732 24.371v7.629l-7.267-7.267h-8.808c-4.781
@@ -29,7 +31,7 @@ class Rant extends Component {
         8.657-8.657h13.604c4.781 0 8.657 3.875 8.657 8.657v7.42c0
         3.922-2.61 7.23-6.186 8.294z"></path>
                         </svg>
-                        23
+                        {rant.commentCount}
                     </div>
                 </div>
             </article>
