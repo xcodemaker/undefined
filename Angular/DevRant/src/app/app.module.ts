dd4a03306e2dbe9864368d98879355923c5aecb9
Dhammika Saman <dhammika_s@3logixs.lk>
2019-02-23 13:27:15 +0530

    angular 9

--- Angular/DevRant/src/app/app.module.ts
+++ Angular/DevRant/src/app/app.module.ts
@@ -7,17 +7,26 @@ import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { LoaderComponent } from './loader/loader.component';
 import { LoaderService } from './loader/loader.service';
+import { Routes, RouterModule } from '@angular/router';
+import { RantDetailsComponent } from './rant-details/rant-details.component';
+
+const appRoutes:Routes=[
+  {path:'rant/:id',component:RantDetailsComponent},
+  {path:'',component:MainContentComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainContentComponent,
-    LoaderComponent
+    LoaderComponent,
+    RantDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [LoaderService ],
   bootstrap: [AppComponent]
