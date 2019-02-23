b9a3f53c3f7659905b66be74a70cebea0c480e53
Dhammika Saman <dhammika_s@3logixs.lk>
2019-02-23 11:59:45 +0530

    angular 6

--- Angular/DevRant/src/app/app.module.ts
+++ Angular/DevRant/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainContentComponent
+    MainContentComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
